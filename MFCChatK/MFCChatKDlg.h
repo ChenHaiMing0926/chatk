8a4295da0e0385c64f3c0d4b319cad11c4c21b58
ChenHaiMing0926 <1739899764@qq.com>
2020-07-20 17:28:59 +0800

    上传基线代码

---
+++ MFCChatK/MFCChatKDlg.h
@@ -0,0 +1,44 @@
+﻿
+// MFCChatKDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMFCChatKDlg 对话框
+class CMFCChatKDlg : public CDialogEx
+{
+// 构造
+public:
+	CMFCChatKDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCCHATK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedAutoBtn();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnBnClickedSendmsgBtn();
+	afx_msg void OnBnClickedAutosendBtn();
+	afx_msg void OnBnClickedClearBtn();
+	afx_msg void OnEnChangeEdit3();
+};
---
+++ MFCChatK/resource.h
@@ -0,0 +1,32 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCChatK.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCHATK_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDC_CONNECT_BTN                 1000
+#define IDC_DISCONNET_BTN               1001
+#define IDC_CLEAR_BTN                   1002
+#define IDC_AUTOSEND_BTN                1004
+#define IDC_EDIT1                       1005
+#define IDC_EDIT2                       1006
+#define IDC_COMBO1                      1007
+#define IDC_LIST1                       1008
+#define IDC_IPADDRESS1                  1009
+#define ID                              1010
+#define IDC_SENDMSG_BTN                 1011
+#define IDC_EDIT3                       1012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
